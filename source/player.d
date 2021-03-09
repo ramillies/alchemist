c50a0a15c06063c2c7abe41673058d5bc0bf2860
ramillies <ramillies@email.cz>
2021-03-09 20:01:40 +0100

    Added inventory and lua bindings to player

--- source/player.d
+++ source/player.d
@@ -2,11 +2,13 @@ import world;
 import resources;
 import util;
 
+import luad.all;
 import dsfml.graphics;
 
 class Player: Drawable
 {
 	size_t x, y;
+	int[string] items;
 	private Sprite sprite;
 
 	this(size_t x, size_t y)
@@ -17,6 +19,35 @@ class Player: Drawable
 		sprite.setTexture(Images.texture("people"));
 		sprite.setTileNumber(5, Vector2u(108, 108));
 		sprite.origin = Vector2f(sprite.getLocalBounds.width/2, sprite.getLocalBounds.height/2);
+
+		foreach(herb; ConfigFiles.get("herbs").keys)
+			items[herb] = 0;
+	}
+
+	void luaPutInto(LuaTable obj)
+	{
+		obj["ptr"] = ptr2string(cast(void *) this);
+		obj["getX"] = delegate size_t(LuaTable t) { return string2ptr!Player(t.get!string("ptr")).x; };
+		obj["getY"] = delegate size_t(LuaTable t) { return string2ptr!Player(t.get!string("ptr")).y; };
+		obj["setPosition"] = delegate void(LuaTable t, size_t x, size_t y)
+		{
+			auto me = string2ptr!Player(t.get!string("ptr"));
+			me.x = x; me.y = y;
+		};
+		obj["giveItems"] = delegate void(LuaTable t, int[string] given)
+		{
+			auto me = string2ptr!Player(t.get!string("ptr"));
+			foreach(key, val; given)
+			{
+				if(key in me.items)
+					me.items[key] += val;
+				else
+					me.items[key] = val;
+
+			}
+		};
+		obj["getItems"] = delegate int[string](LuaTable t) { return string2ptr!Player(t.get!string("ptr")).items; };
+		obj["setItems"] = delegate void(LuaTable t, int[string] x) { string2ptr!Player(t.get!string("ptr")).items = x; };
 	}
 
 	override void draw(RenderTarget target, RenderStates states)
